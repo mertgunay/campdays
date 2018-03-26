9cddcbd2edc0ce1f92d99dcc430227d7be3026e2
Orhan Aykut Kardeş <>
2018-03-27 01:51:21 +0300

    Initial blog model commit

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    updated = models.DateTimeField(auto_now = True, auto_now_add = False)
+    timestampt = models.DateTimeField(auto_now = False, auto_now_add = True)
+
+    def __unicode__(self):
+        return self.title
+
+    def __str__(self):
+        return self.title
