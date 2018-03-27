2376dd116db15f48b1f129671abc78732e611ede
Orhan Aykut Kardeş <aykut.kardes@outlook.com>
2018-03-27 22:19:50 +0300

    blog urls.py added
    test urls confirmed.

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
-from blog.views import blog_home
+from blog.views import (
+    blog_home,
+    blog_detail,
+    blog_list,
+    blog_update,
+    blog_delete,
+)
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('home/', blog_home ),
+    path('home/', blog_home),
+    path('(?P<id>\d+)/', blog_detail),
+    path('list/', blog_list),
+    path('update/', blog_update),
+    path('delete/', blog_delete),
+
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 def blog_home(request):
     return HttpResponse("<h1>Hello There</h1>")
+
+def blog_detail(request, id=None):
+
+    instance = get_object_or_404(Post, id=1)
+    context = {
+    "title": instance.title,
+    "instance": instance,
+    }
+
+    return render(request, "detail.html", context)
+
+def blog_list(request):
+
+    queryset = Post.objects.all()
+    context = {
+    "object_list": queryset,
+    "title": "List"
+    }
+
+    return render(request, "blog.html", context)
+
+def blog_update(request):
+    return HttpResponse("<h1>Update</h1>")
+
+def blog_delete(request):
+    return HttpResponse("<h1>Delete</h1>")
