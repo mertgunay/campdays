c943e807388cef0f455a088ce58adce4ef2ab23f
Orhan Aykut Kardeş <aykut.kardes@outlook.com>
2018-03-29 02:36:06 +0300

    blog: CRUD funcs. completed

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,34 +1,69 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+
+from .forms import PostForm
 from .models import Post
 
 # Create your views here.
 
-def blog_home(request):
-    return HttpResponse("<h1>Hello There</h1>")
+def blog_create(request):
+
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.error(request, "Successfully Created")
+        return HttpResponseRedirect(instance.get_absolut_url())
+    else:
+        messages.error(request, "Not Successfully Created")
+
+    context = {
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
 
 def blog_detail(request, id=None):
 
-    instance = get_object_or_404(Post, id=1)
+    instance = get_object_or_404(Post, id=id)
     context = {
-    "title": instance.title,
-    "instance": instance,
+        "title": instance.title,
+        "instance": instance,
     }
 
-    return render(request, "detail.html", context)
+    return render(request, "post_detail.html", context)
 
 def blog_list(request):
 
     queryset = Post.objects.all()
     context = {
-    "object_list": queryset,
-    "title": "List"
+        "object_list": queryset,
+        "title": "List"
     }
 
     return render(request, "blog.html", context)
 
-def blog_update(request):
-    return HttpResponse("<h1>Update</h1>")
+def blog_update(request, id=None):
 
-def blog_delete(request):
-    return HttpResponse("<h1>Delete</h1>")
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.error(request, "Post Updated")
+        return HttpResponseRedirect(instance.get_absolut_url())
+
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "form": form,
+    }
+
+    return render(request, "post_form.html", context)
+
+def blog_delete(request, id=None):
+
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.error(request, "Post Deleted")
+    return redirect("blog:list")
