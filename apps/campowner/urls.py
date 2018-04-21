0537ccc126542ff1414fb2784ad52902552703fc
mertgunay <mertgunay8@gmail.com>
2018-04-21 16:52:56 +0300

    Camp Owner kayıt olma
    Normal bir kullanıcı kamp sahibi sayfası için istek gönderebilir

--- apps/campowner/urls.py
+++ apps/campowner/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from campowner.views import pending_owners
+from campowner.views import pending_owners, CampOwnerCreateView
 
 app_name = 'campowner'
 
 urlpatterns = [
     path('pending_owners', pending_owners, name='pending_owners'),
+    path('campowner/', CampOwnerCreateView.as_view(), name="create")
 ]
