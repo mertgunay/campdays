da74790f622ad5e095a99d5bf798876550d84db2
Batuhan Kurt <batuhan.kurt@isik.edu.tr>
2018-04-25 15:09:52 +0300

    Modifying admin functions ( banned users , statistic page )
    Modifying admin functions ( banned users , statistic page )

--- apps/campowner/urls.py
+++ apps/campowner/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from campowner.views import pending_owners, CampOwnerCreateView
+from campowner.views import pending_owners, CampOwnerCreateView, accept_or_reject
 
 app_name = 'campowner'
 
 urlpatterns = [
     path('pending_owners', pending_owners, name='pending_owners'),
-    path('campowner/', CampOwnerCreateView.as_view(), name="create")
+    path('campowner/', CampOwnerCreateView.as_view(), name="create"),
+    path('accept_or_reject', accept_or_reject, name="accept_or_reject"),
 ]
--- apps/campowner/views.py
+++ apps/campowner/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, reverse
+from django.shortcuts import render, reverse, get_object_or_404
+from django.http import HttpResponseRedirect
 from django.views.generic import CreateView
 from campowner.forms import CampOwnerRegisterForm
+from campowner.models import CampOwner
 
 class CampOwnerCreateView(CreateView):
     template_name = 'accounts/registration/register_campowner.html'
@@ -15,4 +17,25 @@ class CampOwnerCreateView(CreateView):
         return reverse('home')
 
 def pending_owners(request):
-    return render(request, 'campowner/pending_owners.html', {})
+    queryset = CampOwner.objects.filter(is_pending=True)
+    context = {
+        'object_list': queryset
+    }
+    return render(request, 'campowner/pending_owners.html', context)
+
+
+def accept_or_reject(request):
+    if request.method == 'POST':
+        if 'accept' in request.POST:
+            id = request.POST.get("accept")
+            campowner = get_object_or_404(CampOwner, id=id)
+            campowner.is_active = True
+            campowner.is_pending = False
+            campowner.save()
+        elif 'reject' in request.POST:
+            id = request.POST.get("reject")
+            campowner = get_object_or_404(CampOwner, id=id)
+            campowner.is_active = False
+            campowner.is_pending = False
+            campowner.save()
+    return HttpResponseRedirect("/superuser/pending_owners")
