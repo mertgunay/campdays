bbf8072ea5fc09d7af06d93c5fc8d3a95eb581d8
Aykut <aykut.kardes@outlook.com>
2018-05-06 17:25:08 +0300

    blog page embedded into campdetail page

--- apps/campowner/urls.py
+++ apps/campowner/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from campowner.views import pending_owners, CampOwnerCreateView, accept_or_reject
+from campowner.views import pending_owners, CampOwnerCreateView, accept_or_reject, campowner
 
 app_name = 'campowner'
 
 urlpatterns = [
     path('pending_owners', pending_owners, name='pending_owners'),
-    path('campowner/', CampOwnerCreateView.as_view(), name="create"),
+    path('campowner', CampOwnerCreateView.as_view(), name="create"),
     path('accept_or_reject', accept_or_reject, name="accept_or_reject"),
+    path('<pk>', campowner, name="detail"),
+
 ]
