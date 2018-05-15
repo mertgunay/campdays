5f0028f92561d078be9552cc0d14bcbb18372187
Muhammed Ali <malialtinel@gmail.com>
2018-05-15 15:13:21 +0300

    Kamp Listeleme-Search-Pagination

--- apps/campowner/urls.py
+++ apps/campowner/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from campowner.views import pending_owners, CampOwnerCreateView, accept_or_reject, campowner
+from campowner.views import pending_owners, CampOwnerCreateView, accept_or_reject, campowner, camp_owners
 
 app_name = 'campowner'
 
@@ -8,6 +8,10 @@ urlpatterns = [
     path('pending_owners', pending_owners, name='pending_owners'),
     path('campowner', CampOwnerCreateView.as_view(), name="create"),
     path('accept_or_reject', accept_or_reject, name="accept_or_reject"),
+    path('camp_owners', camp_owners, name='camp_owners'),
+
+    ## her zaman en aşağıda olmalı. aksi halde bozulur
     path('<pk>', campowner, name="detail"),
+   
 
 ]
