5e694f7a24eb122a86495d55a3f181bbf6a52eec
Batuhan Kurt <batuhan.kurt@isik.edu.tr>
2018-05-23 19:35:02 +0300

    23213123

--- apps/ban/views.py
+++ apps/ban/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
 from ban.models import BannedUser
+from booking.models import Reservation
 import datetime
 
 User = get_user_model()
@@ -10,8 +11,10 @@ def stat(request):
     queryset = BannedUser.objects.all()
     date_from = datetime.datetime.now() - datetime.timedelta(hours=1)
     last_logins = User.objects.filter(last_login__gte=date_from)
+    reservation_list = Reservation.objects.filter(timestamp__gte=date_from)
     context = {
         'banned_users': queryset,
         'object_list': last_logins,
+        'reservation_list': reservation_list,
     }
     return render(request, "ban/stat.html", context)
